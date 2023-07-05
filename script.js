777b20c17d242f331ea186f961e917540b857082
Isac <isac.mamede14@gmail.com>
2023-07-04 21:32:37 -0300

    working

---
+++ script.js
@@ -0,0 +1,26 @@
+function multiply(){
+    let n1 = document.querySelector('#n1')
+    let operation = document.querySelector('#operation')
+    let n2 = document.querySelector('#n2')
+    let div = document.querySelector('div')
+    n1 = n1.value
+    operation = operation.value
+    n2 = n2.value
+    
+    // Verificando qual a operação requerida
+    if(operation == '-'){
+        result = parseInt(n1) - parseInt(n2) 
+    }
+    else if(operation == '+'){
+        result = parseInt(n1) + parseInt(n2) 
+    }
+    else if(operation == '*'){
+        result = parseInt(n1) * parseInt(n2)
+    }
+    else if(operation == '/'){
+        result = parseInt(n1) / parseInt(n2)
+    }
+
+    console.log(result)
+    div.innerHTML = result
+}
